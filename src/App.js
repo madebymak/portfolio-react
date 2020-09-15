a74154865ee6f58b088b45b1882db01c15ccc6d9
madebymak <mr.jmak@gmail.com>
2020-09-15 16:57:39 -0700

    Add components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Greet from './components/welcome';
+
+class App extends Component {
+    render() {
+        return (
+            <div> <Greet /></div>
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-/*jshint esversion: 6 */
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
 
-ReactDOM.render("Hello World", document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
