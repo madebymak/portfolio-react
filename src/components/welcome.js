efe80312041bfb09b08c615422e6828f9aaaad4b
madebymak <mr.jmak@gmail.com>
2020-09-16 15:32:58 -0700

    Add Welcome section

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,10 +1,29 @@
 import React from 'react';
-
-// const Greet = () => <h1>Hello World 123</h1>
+import './welcome.css';
 
 function Greet() {
     return (
-        <h1>Hello World</h1>
+        <div className='row'>
+            <div className='section main'>
+                <div className='flex'>
+                    <div className='col-4'>
+                        {/* TODO: add image */}
+                        <img className='profile-circle zoomIn' src='./public/img/profile_square.jpg' alt='' />
+                    </div>
+                    <div className='col-8 intro slideInRight'>
+                        <h1>Hi there,<br /> My name is Jacky and I am a <span className='blue-text'>web developer</span>.</h1>
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className='col-12 arrow slideInUp'>
+                        {/* TODO: install fontawesome for react */}
+                        <a href='#work'>arrow icon<i className='arrow-color arrow-animate fa fa-arrow-down fa-2x' aria-hidden='true'></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
+
 export default Greet
